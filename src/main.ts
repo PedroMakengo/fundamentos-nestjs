f80aec9a05eaed81af60374e051c4628190e361c
PedroMakengo <pedromakengomiguel@gmail.com>
2024-01-15 23:34:20 +0100

    Aprendendo sobre os fundamentos do nestjs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-
+  app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/middlewares/user-id-check.middleware.ts
@@ -0,0 +1,11 @@
+import { BadGatewayException, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+export class UserIdCheckMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    if (isNaN(Number(req.params.id)) || Number(req.params.id) <= 0) {
+      throw new BadGatewayException('ID inválido!');
+    }
+    next();
+  }
+}
