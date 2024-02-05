254155a355331f299cba4d5e18a017f41be5ca3b
PedroMakengo <pedromakengomiguel@gmail.com>
2024-02-05 23:19:08 +0100

    Resolvido os erros com as minhas migrations

---
+++ typeorm/data-source.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '2024Makengo',
+  database: 'api',
+  migrations: [`${__dirname}/migrations/**/*.ts`],
+});
+
+export default dataSource;
