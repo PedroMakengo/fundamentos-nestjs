99e1523574d2632f844e7c535bfc3fb34a9d9032
PedroMakengo <pedromakengomiguel@gmail.com>
2024-03-08 08:30:23 +0100

    Adicionando os tests no controller

---
+++ src/user/user.controller.spec.ts
@@ -0,0 +1,65 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { userServiceMock } from '../testing/user-service.mock';
+import { AuthGuard } from '../guards/auth.guard';
+import { guardMock } from '../testing/guard.mock';
+import { RoleGuard } from '../guards/role.guard';
+import { UserService } from './user.service';
+import { createUserDTO } from '../testing/create-user.dto.mock';
+import { userEntityList } from '../testing/user-entity-list.mock';
+import { updatePutUserDTO } from '../testing/update-put-user.dto.mock';
+import { updatePatchUserDTO } from '../testing/update-patch-user.dto';
+
+describe('UserController', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [userServiceMock],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(guardMock)
+      .overrideGuard(RoleGuard)
+      .useValue(guardMock)
+      .compile();
+
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+  });
+
+  test('Validar a definição', () => {
+    expect(userController).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('Create', () => {
+    test('Create method', async () => {
+      const result = await userController.create(createUserDTO);
+
+      expect(result).toEqual(userEntityList[0]);
+    });
+
+    test('Show method', async () => {
+      const result = await userController.show(1);
+
+      expect(result).toEqual(userEntityList[0]);
+    });
+
+    test('Update method', async () => {
+      const result = await userController.update(1, updatePutUserDTO);
+      expect(result).toEqual(userEntityList[0]);
+    });
+
+    test('UpdatePartial method', async () => {
+      const result = await userController.updatePartial(1, updatePatchUserDTO);
+      expect(result).toEqual(userEntityList[0]);
+    });
+
+    test('Delete method', async () => {
+      const result = await userController.delete(1);
+      expect(result).toEqual({ success: true });
+    });
+  });
+});
